0ff447951b7d7c38935325b96bd5be4aa2976f3e
Tayanne <tayh.snovaes@gmail.com>
2021-11-20 13:43:38 -0300

    add sql queries to algorithm and programs

--- src/controllers/algorithm.js
+++ src/controllers/algorithm.js
@@ -1,30 +1,34 @@
 import { connection } from "../data/connection.js";
 
 async function gerarComputador(req, res) {
-   const computador = {
-     id: req.body.id,
-     nome: req.body.nome,
-     rank: req.body.rank,
-     categorias: {
-       id: req.body.cat.id,
-       categoria: req.body.cat.nome
-     },
-     nvl_consumo: {
-       id: req.body.nvl.id,
-       nivel: req.body.nvl.nivel
-     },
-     valor_minimo: req.body.valor_minimo,
-     valor_maximo: req.body.valor_maximo,
-     sem_valor: req.body.sem_valor
-   }
+  const computador = {
+    programas: {
+      id: req.body.programas.id,
+      nome: req.body.programas.nome,
+      rank: req.body.programas.rank,
+      categorias: {
+        id: req.body.cat.id,
+        categoria: req.body.cat.nome,
+      },
+      nvl_consumo: {
+        id: req.body.nvl.id,
+        nivel: req.body.nvl.nivel,
+      },
+    },
+    valor_minimo: req.body.valor_minimo,
+    valor_maximo: req.body.valor_maximo,
+    sem_valor: req.body.sem_valor,
+  };
 
-   try {
-     
+  try {
+    const data = await connection("computador").insert({
 
-      return res.send().status(201)
-   } catch (error) {
-      return res.status(400).json(error.message);
-   }
+    });
+
+    return res.send(data).status(201);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
 }
 
-export { gerarComputador }
+export { gerarComputador };
--- src/controllers/programs.js
+++ src/controllers/programs.js
@@ -1,25 +1,42 @@
 import { connection } from "../data/connection.js";
 
-async function getAllCategorias(req, res) {
-      try {
-        const data = await connection.select("id", "categoria").from("categorias");
-        return res.send(data).status(200);
-      } catch (error) {
-         return res.status(400).json(error.message);
-      }
-    }
-
-async function getProgramasByCategoriaId(req, res) {
-   const id = req.params.id
-
-   try {
-     const data = await connection.select(``)
-
-
-     res.send(data).status(200)
-   } catch (error) {
-      return res.status(400).json(error.message);
-   }
+async function getAllCategorias(_, res) {
+  try {
+    const data = await connection.select("id", "categoria").from("categorias");
+    return res.send(data).status(200);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
 }
 
-export { getAllCategorias, getProgramasByCategoriaId }
+async function getProgramasByCategoriaId(req, res) {
+  const id = req.params.id;
+
+  try {
+    const data = await connection
+      .table("programas")
+      .innerJoin("categorias", "categorias.id", "=", "programas.id_categoria")
+      .innerJoin(
+        "nivel_consumo",
+        "nivel_consumo.id",
+        "=",
+        "programas.id_nivel_consumo"
+      )
+      .select(
+        "programas.id",
+        "programas.nome",
+        "programas.rank",
+        "categorias.id",
+        "categorias.categoria",
+        "nivel_consumo.id",
+        "nivel_consumo.nivel"
+      )
+      .where('programas.id_categoria', id);
+
+    res.send(data).status(200);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+export { getAllCategorias, getProgramasByCategoriaId };
