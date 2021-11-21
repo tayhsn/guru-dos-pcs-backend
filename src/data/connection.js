0083cd0d8afd6f421453c31be291cc02a5b94046
Tayanne <tayh.snovaes@gmail.com>
2021-11-21 09:30:49 -0300

    change port on connection

--- src/data/connection.js
+++ src/data/connection.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connection = knex({
-   client: "mysql",
-   connection: {
-     host: process.env.DB_HOST,
-     port: process.env.DB_PORT,
-     user: process.env.DB_USER,
-     password: process.env.DB_PASS,
-     database: process.env.DB_NAME,
-   },
- });
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+});
